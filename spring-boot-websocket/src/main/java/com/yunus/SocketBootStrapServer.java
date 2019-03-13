124c499b7ebd6c28c9ce8a6879772c82adf8e491
gaoyunfeng <gaoyunfeng@lanxum.com>
2019-03-13 15:10:27 +0800

    websocket spring boot

--- spring-boot-websocket/src/main/java/com/yunus/SocketBootStrapServer.java
+++ spring-boot-websocket/src/main/java/com/yunus/SocketBootStrapServer.java
@@ -2,12 +2,14 @@ package com.yunus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @Author: gaoyunfeng
  * @date: 2019/3/13
  */
 @SpringBootApplication
+@EnableScheduling
 public class SocketBootStrapServer {
     public static void main(String[] args) {
         SpringApplication.run(SocketBootStrapServer.class, args);
