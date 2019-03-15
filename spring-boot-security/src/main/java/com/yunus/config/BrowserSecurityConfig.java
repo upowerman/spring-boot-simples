27d807c935bbc29fdd1d98b727d82507902c6e29
gaoyunfeng <gaoyunfeng@lanxum.com>
2019-03-15 11:31:54 +0800

    spring security demo

--- spring-boot-security/src/main/java/com/yunus/config/BrowserSecurityConfig.java
+++ spring-boot-security/src/main/java/com/yunus/config/BrowserSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.yunus.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @Author: gaoyunfeng
@@ -13,14 +16,26 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 表单方式认证
-        http.formLogin()
+        http.formLogin() // 表单登录
+                // http.httpBasic() // HTTP Basic
+                // 登录跳转 URL
+                .loginPage("/statics/login.html")
+                // 处理表单登录 URL
+                .loginProcessingUrl("/login")
                 .and()
                 // 授权配置
                 .authorizeRequests()
+                // 登录跳转 URL 无需认证
+                .antMatchers("/statics/**").permitAll()
                 // 所有请求
                 .anyRequest()
                 // 都需要认证
-                .authenticated();
+                .authenticated()
+                .and().csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ spring-boot-security/src/main/java/com/yunus/config/WebAppConfig.java
@@ -0,0 +1,17 @@
+package com.yunus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: gaoyunfeng
+ * @date: 2019/3/15
+ */
+@Configuration
+public class WebAppConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
+    }
+}
