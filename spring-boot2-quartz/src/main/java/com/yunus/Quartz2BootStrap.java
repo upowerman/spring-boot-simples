3ce015da3c743a00a6a9a493bacea292bc897234
gaoyunfeng <bogon@apple.com>
2019-06-07 15:56:53 +0800

    spring-boot-quartz

---
+++ spring-boot2-quartz/src/main/java/com/yunus/Quartz2BootStrap.java
@@ -0,0 +1,11 @@
+package com.yunus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quartz2BootStrap {
+    public static void main(String[] args) {
+        SpringApplication.run(Quartz2BootStrap.class, args);
+    }
+}
