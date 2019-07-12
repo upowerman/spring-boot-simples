c81bafa78d5dffe1e57f4e40927788381d8dc704
gaoyunfeng <bogon@apple.com>
2019-07-12 14:07:31 +0800

    kafka

---
+++ spring-boot-mq-kafka/src/main/java/com/yunus/KafkaBootStrapServer.java
@@ -0,0 +1,14 @@
+package com.yunus;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBootStrapServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaBootStrapServer.class, args);
+    }
+
+}
