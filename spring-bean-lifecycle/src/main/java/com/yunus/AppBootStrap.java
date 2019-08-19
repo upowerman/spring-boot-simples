5fed25eb084486319756f32c6ac5521c832a56af
gaoyunfeng <bogon@apple.com>
2019-08-19 14:43:42 +0800

    activiti demo

--- spring-bean-lifecycle/src/main/java/com/yunus/AppBootStrap.java
+++ spring-bean-lifecycle/src/main/java/com/yunus/AppBootStrap.java
@@ -1,11 +1,14 @@
-package com.yunus.context;
+package com.yunus;
 
+import com.yunus.context.LifeCycleConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Author: gaoyunfeng
  * @date: 2019/3/18
  */
+@ComponentScan(basePackages = "com.yunus.bean")
 public class AppBootStrap {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
