94eed246beb3144e48bf03b8bda380adc15e64c2
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-11-05 16:11:51 +0800

    GraphQl

---
+++ spring-boot-GraphQL/src/main/java/com/yunus/GraphQlBootStrap.java
@@ -0,0 +1,16 @@
+package com.yunus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoyunfeng
+ * @Description:
+ * @date 2020/11/5 15:30
+ */
+@SpringBootApplication
+public class GraphQlBootStrap {
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlBootStrap.class,args);
+    }
+}
