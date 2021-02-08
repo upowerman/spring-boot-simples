5ab4968f3889ceec38074e6f78b80b11bf78d660
gaoyunfeng <gaoyunfeng200420@credithc.com>
2021-02-08 14:13:11 +0800

    feat(spring-boot-security-auth2): spring-boot-security-password auth2 授权码模式

--- spring-boot-security/spring-boot-security-auth2-code-resourceServer/src/main/java/com/yunus/client/controller/CallbackController.java
+++ spring-boot-security/spring-boot-security-auth2-code-resourceServer/src/main/java/com/yunus/client/controller/CallbackController.java
@@ -1,23 +1,20 @@
-package com.yunus.controller;
+package com.yunus.client.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.OAuth2ClientProperties;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author M4500
- */
 @RestController
 @RequestMapping("/")
-public class LoginController {
+public class CallbackController {
 
     @Autowired
     private OAuth2ClientProperties oauth2ClientProperties;
@@ -25,20 +22,21 @@ public class LoginController {
     @Value("${security.oauth2.access-token-uri}")
     private String accessTokenUri;
 
-    @PostMapping("/login")
-    public OAuth2AccessToken login(@RequestParam("username") String username,
-                                   @RequestParam("password") String password) {
-        // 创建 ResourceOwnerPasswordResourceDetails 对象
-        ResourceOwnerPasswordResourceDetails resourceDetails = new ResourceOwnerPasswordResourceDetails();
+    @GetMapping("/callback")
+    public OAuth2AccessToken login(@RequestParam("code") String code) {
+        // 创建 AuthorizationCodeResourceDetails 对象
+        AuthorizationCodeResourceDetails resourceDetails = new AuthorizationCodeResourceDetails();
         resourceDetails.setAccessTokenUri(accessTokenUri);
         resourceDetails.setClientId(oauth2ClientProperties.getClientId());
         resourceDetails.setClientSecret(oauth2ClientProperties.getClientSecret());
-        resourceDetails.setUsername(username);
-        resourceDetails.setPassword(password);
         // 创建 OAuth2RestTemplate 对象
         OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails);
-        restTemplate.setAccessTokenProvider(new ResourceOwnerPasswordAccessTokenProvider());
-        // 获取访问令牌
+        // 设置 code
+        restTemplate.getOAuth2ClientContext().getAccessTokenRequest().setAuthorizationCode(code);
+        // 通过这个方式，设置 redirect_uri 参数
+        restTemplate.getOAuth2ClientContext().getAccessTokenRequest().setPreservedState("http://127.0.0.1:9090/callback");
+        restTemplate.setAccessTokenProvider(new AuthorizationCodeAccessTokenProvider());
+        // 获取访问资源服务器的令牌
         return restTemplate.getAccessToken();
     }
 
---
+++ spring-boot-security/spring-boot-security-auth2-password-resourceServer/src/main/java/com/yunus/third/client/pacakge-info.java
@@ -0,0 +1,4 @@
+/**
+ * 模拟第三方客户端 后台
+ */
+package com.yunus.third.client;
