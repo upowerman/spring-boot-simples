d1cc6571cd5d4c0bcd76f6e514cdf2bd800ded7b
gaoyunfeng <gaoyunfeng200420@credithc.com>
2021-02-08 10:55:44 +0800

    feat(spring-boot-security-auth2): spring-boot-security-password auth2 密码模式
    密码模式，用户向客户端提供自己的用户名和密码。客户端使用这些信息，向授权服务器索要授权。

---
+++ spring-boot-security/spring-boot-security-auth2-password-authServer/src/main/java/com/yunus/config/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,55 @@
+package com.yunus.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @author gaoyunfeng
+ * @Description:
+ * @date 2021/2/7 12:16
+ */
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    /**
+     * 用户认证 Manager
+     */
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.checkTokenAccess("isAuthenticated()");
+//        oauthServer.tokenKeyAccess("isAuthenticated()")
+//                .checkTokenAccess("isAuthenticated()");
+//        oauthServer.tokenKeyAccess("permitAll()")
+//                .checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("clientapp").secret("112233") // Client 账号、密码。
+                .authorizedGrantTypes("password") // 密码模式
+                .scopes("read_userinfo", "read_contacts") // 可授权的 Scope
+//                .and().withClient() // 可以继续配置新的 Client
+        ;
+    }
+}
---
+++ spring-boot-security/spring-boot-security-auth2-password-resourceServer/src/main/java/com/yunus/ResourceServerApp.java
@@ -0,0 +1,15 @@
+package com.yunus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoyunfeng
+ * @date 2021/2/4 15:48
+ */
+@SpringBootApplication
+public class ResourceServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApp.class, args);
+    }
+}
