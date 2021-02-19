6fe5b76f1f8998bc1f75a1db40e5d7662f6dc99f
gaoyunfeng <gaoyunfeng200420@credithc.com>
2021-02-19 13:53:31 +0800

    feat(spring-boot-job): spring-boot-xxl-job 分布式定时任务 xxl-job
    xxl-job 简单demo

---
+++ spring-boot-job/spring-boot-xxl-job/src/main/java/com/yunus/XxlJobApp.java
@@ -0,0 +1,16 @@
+package com.yunus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoyunfeng
+ * @Description:
+ * @date 2021/2/4 15:48
+ */
+@SpringBootApplication
+public class XxlJobApp {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobApp.class, args);
+    }
+}
