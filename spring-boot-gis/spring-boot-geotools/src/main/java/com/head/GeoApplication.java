b64e7ae800f81a982f9e431664eb9f37f0660f35
gaoyunfeng <gyf718@163.com>
2023-03-03 08:56:44 +0800

    add geotool

---
+++ spring-boot-gis/spring-boot-geotools/src/main/java/com/head/GeoApplication.java
@@ -0,0 +1,7 @@
+package com.head;
+
+/**
+ * @author gaoyunfeng
+ */
+public class GeoApplication {
+}
---
+++ spring-boot-gis/spring-boot-geotools/src/main/java/com/head/demo1/Quickstart.java
@@ -0,0 +1,42 @@
+package com.head.demo1;
+
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.data.JFileDataStoreChooser;
+
+import java.io.File;
+
+/**
+ * @author gaoyunfeng
+ */
+public class Quickstart {
+
+    public static void main(String[] args) throws Exception {
+        // display a data store file chooser dialog for shapefiles
+        File file = JFileDataStoreChooser.showOpenFile("shp", null);
+        if (file == null) {
+            return;
+        }
+
+        FileDataStore store = FileDataStoreFinder.getDataStore(file);
+        SimpleFeatureSource featureSource = store.getFeatureSource();
+
+        // Create a map content and add our shapefile to it
+        MapContent map = new MapContent();
+        map.setTitle("Quickstart");
+
+        Style style = SLD.createSimpleStyle(featureSource.getSchema());
+        Layer layer = new FeatureLayer(featureSource, style);
+        map.addLayer(layer);
+
+        // Now display the map
+        JMapFrame.showMap(map);
+    }
+}
